207dae05f0792bfb9b7d555bacbae23f8efa80f6
daryaami <daryaamii@mail.ru>
2024-06-23 18:28:07 +0300

    upd

--- planner/urls.py
+++ planner/urls.py
@@ -6,6 +6,7 @@ app_name = "planner"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create_event/", views.create_event, name="create_event"),
+    path("get_events/", views.get_events, name="het_events"),
     # path("delete_event/", views.delete_event, name='delete_event'),
     # path("get_event/", views.get_event, name='get_event'),
     # path("update_event/", views.update_event, name='update_event'),

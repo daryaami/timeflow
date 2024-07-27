bec7e752479403f34beb2ecd9b79d5184e7e0c0f
daryaami <daryaamii@mail.ru>
2024-07-28 01:25:16 +0800

    fix get_user_info

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from googleapiclient.discovery import build
 from django.http import JsonResponse
 from users.models import GoogleCredentials, UserCalendar, CustomUser, Hours
 from tasks.models import Task
-from tasks.utils import get_user_tasks
+from tasks.utils import get_user_tasks_json
 from .utils import get_user_hours_json
 
 
@@ -18,6 +18,6 @@ def get_user_info(request):
     user_info = {
         "profile": user.get_profile_json(),
         "hours": get_user_hours_json(user=user),
-        "tasks": get_user_tasks(user=user)
+        "tasks": get_user_tasks_json(user=user)
     }
     return JsonResponse({"user_data": user_info})
